19911a48c27acc13f7a56da3363eea7ddecbb5ce
Stefan van Herwijnen <stefan@devliert.info>
2021-09-20 11:20:54 +0200

    refactor: move baseConfig from build to index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
+import vuePlugin from '@vitejs/plugin-vue'
+import { QuasarPlugin } from '@stefanvh/quasar-app-vite'
+import { resolve } from 'path'
+
 export * from '@stefanvh/quasar-app-vite/vite-plugin-quasar'
+
+export const baseConfig = ({
+    cliDir,
+    srcDir,
+    appDir,
+    ssr
+  }: {
+    cliDir: string,
+    srcDir: string,
+    appDir: string, 
+    ssr?:  'client' | 'server' | 'ssg' }
+  ) => ({
+    root: cliDir,
+    plugins: [
+      vuePlugin(),
+      QuasarPlugin({
+        ssr: ssr
+      })
+    ],
+    resolve: {
+      alias: [
+        { find: 'src', replacement: srcDir },
+        { find: 'dist', replacement: resolve('dist') },
+        { find: 'quasar', replacement: resolve(appDir, 'node_modules', 'quasar') }
+      ]
+    },
+    ssr: {
+      noExternal: ssr === 'server' ? ['quasar'] : []
+    }
+  })
+  
