5276eced0f2f31d4322b395c1be5f3d2f98c16f5
Stefan van Herwijnen <stefan@devliert.info>
2022-01-22 17:39:12 +0100

    fix: fix csr entry point

--- packages/quasar-app-vite/csr/entry.ts
+++ packages/quasar-app-vite/csr/entry.ts
@@ -1,8 +1,8 @@
 import { createApp } from '../main'
 
-const { app, router } = createApp()
-
-// wait until router is ready before mounting to ensure hydration match
-router.isReady().then(() => {
-  app.mount('#app')
+createApp().then(({ app, router }) => {
+  // wait until router is ready before mounting to ensure hydration match
+  router.isReady().then(() => {
+    app.mount('#app')
+  })
 })
